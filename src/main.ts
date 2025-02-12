8173ef313cf66f518d24e2538da05e81ffc5b9d2
Belhajikram <ikrambelhaj19@gmail.com>
2025-02-12 14:29:57 +0100

    Enable railway url

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   dotenv.config();
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://notesappfront-production.up.railway.app',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
